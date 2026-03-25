6d9ec3c1607c9581bff9cca653d6a27915dec112
Kimmypara <kparascandalo@gmail.com>
2026-03-25 14:06:52 +0100

    SQL
    +SQL export
    +ERD

---
+++ Database export/be_mine.sql
@@ -0,0 +1,544 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 25, 2026 at 01:55 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `be_mine`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking`
+--
+
+CREATE TABLE `booking` (
+  `booking_id` int(11) NOT NULL,
+  `wedding_plan_id` int(11) NOT NULL,
+  `vendor_id` int(11) NOT NULL,
+  `task_id` int(11) NOT NULL,
+  `booking_status_id` int(11) NOT NULL,
+  `booked_price` decimal(10,2) DEFAULT NULL,
+  `booked_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking_status`
+--
+
+CREATE TABLE `booking_status` (
+  `booking_status_id` int(11) NOT NULL,
+  `booking_status_name` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat`
+--
+
+CREATE TABLE `chat` (
+  `chat_id` int(11) NOT NULL,
+  `is_group` tinyint(1) DEFAULT NULL,
+  `chat_name` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat_member`
+--
+
+CREATE TABLE `chat_member` (
+  `chat_member_id` int(11) NOT NULL,
+  `chat_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat_message`
+--
+
+CREATE TABLE `chat_message` (
+  `chat_message_id` int(11) NOT NULL,
+  `chat_id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  `message_content` text DEFAULT NULL,
+  `sent_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guest`
+--
+
+CREATE TABLE `guest` (
+  `guest_id` int(11) NOT NULL,
+  `wedding_plan_id` int(11) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `guest_name` varchar(255) DEFAULT NULL,
+  `guest_surname` varchar(255) DEFAULT NULL,
+  `rsvp_status` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `our_wedding`
+--
+
+CREATE TABLE `our_wedding` (
+  `our_wedding_id` int(11) NOT NULL,
+  `profile_image` varchar(500) DEFAULT NULL,
+  `wedding_plan_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reset_password`
+--
+
+CREATE TABLE `reset_password` (
+  `reset_password_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `reset_token` varchar(255) DEFAULT NULL,
+  `expires_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `role_id` int(11) NOT NULL,
+  `role_name` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings_theme`
+--
+
+CREATE TABLE `settings_theme` (
+  `settings_theme_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `dark_mode` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task`
+--
+
+CREATE TABLE `task` (
+  `task_id` int(11) NOT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `task_name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password_hash` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  `is_active` tinyint(1) DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vendor`
+--
+
+CREATE TABLE `vendor` (
+  `vendor_id` int(11) NOT NULL,
+  `vendor_name` varchar(500) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wedding_plan`
+--
+
+CREATE TABLE `wedding_plan` (
+  `wedding_plan_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `user_nickname` varchar(100) DEFAULT NULL,
+  `partner_nickname` varchar(100) DEFAULT NULL,
+  `wedding_date` date DEFAULT NULL,
+  `guest_count` int(11) DEFAULT NULL,
+  `budget` decimal(10,0) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wedding_plan_task`
+--
+
+CREATE TABLE `wedding_plan_task` (
+  `wedding_plan_task_id` int(11) NOT NULL,
+  `wedding_plan_id` int(11) DEFAULT NULL,
+  `task_id` int(11) DEFAULT NULL,
+  `is_selected` tinyint(1) DEFAULT NULL,
+  `completed_at` datetime DEFAULT NULL,
+  `is_completed` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`booking_id`),
+  ADD KEY `wedding_plan_id` (`wedding_plan_id`),
+  ADD KEY `vendor_id` (`vendor_id`),
+  ADD KEY `task_id` (`task_id`),
+  ADD KEY `booking_status_id` (`booking_status_id`);
+
+--
+-- Indexes for table `booking_status`
+--
+ALTER TABLE `booking_status`
+  ADD PRIMARY KEY (`booking_status_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`chat_id`);
+
+--
+-- Indexes for table `chat_member`
+--
+ALTER TABLE `chat_member`
+  ADD PRIMARY KEY (`chat_member_id`),
+  ADD KEY `chat_id` (`chat_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `chat_message`
+--
+ALTER TABLE `chat_message`
+  ADD PRIMARY KEY (`chat_message_id`),
+  ADD KEY `chat_id` (`chat_id`),
+  ADD KEY `sender_id` (`sender_id`);
+
+--
+-- Indexes for table `guest`
+--
+ALTER TABLE `guest`
+  ADD PRIMARY KEY (`guest_id`),
+  ADD KEY `wedding_plan_id` (`wedding_plan_id`);
+
+--
+-- Indexes for table `our_wedding`
+--
+ALTER TABLE `our_wedding`
+  ADD PRIMARY KEY (`our_wedding_id`),
+  ADD KEY `wedding_plan_id` (`wedding_plan_id`);
+
+--
+-- Indexes for table `reset_password`
+--
+ALTER TABLE `reset_password`
+  ADD PRIMARY KEY (`reset_password_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`role_id`);
+
+--
+-- Indexes for table `settings_theme`
+--
+ALTER TABLE `settings_theme`
+  ADD PRIMARY KEY (`settings_theme_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `task`
+--
+ALTER TABLE `task`
+  ADD PRIMARY KEY (`task_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `email_2` (`email`),
+  ADD KEY `role_id` (`role_id`);
+
+--
+-- Indexes for table `vendor`
+--
+ALTER TABLE `vendor`
+  ADD PRIMARY KEY (`vendor_id`),
+  ADD KEY `category_id` (`category_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `wedding_plan`
+--
+ALTER TABLE `wedding_plan`
+  ADD PRIMARY KEY (`wedding_plan_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `wedding_plan_task`
+--
+ALTER TABLE `wedding_plan_task`
+  ADD PRIMARY KEY (`wedding_plan_task_id`),
+  ADD KEY `wedding_plan_id` (`wedding_plan_id`),
+  ADD KEY `task_id` (`task_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `booking_status`
+--
+ALTER TABLE `booking_status`
+  MODIFY `booking_status_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `chat_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `chat_member`
+--
+ALTER TABLE `chat_member`
+  MODIFY `chat_member_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `chat_message`
+--
+ALTER TABLE `chat_message`
+  MODIFY `chat_message_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `guest`
+--
+ALTER TABLE `guest`
+  MODIFY `guest_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `our_wedding`
+--
+ALTER TABLE `our_wedding`
+  MODIFY `our_wedding_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reset_password`
+--
+ALTER TABLE `reset_password`
+  MODIFY `reset_password_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `settings_theme`
+--
+ALTER TABLE `settings_theme`
+  MODIFY `settings_theme_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `task`
+--
+ALTER TABLE `task`
+  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `vendor`
+--
+ALTER TABLE `vendor`
+  MODIFY `vendor_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `wedding_plan`
+--
+ALTER TABLE `wedding_plan`
+  MODIFY `wedding_plan_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `wedding_plan_task`
+--
+ALTER TABLE `wedding_plan_task`
+  MODIFY `wedding_plan_task_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `booking`
+--
+ALTER TABLE `booking`
+  ADD CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`wedding_plan_id`) REFERENCES `wedding_plan` (`wedding_plan_id`),
+  ADD CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`vendor_id`) REFERENCES `vendor` (`vendor_id`),
+  ADD CONSTRAINT `booking_ibfk_3` FOREIGN KEY (`task_id`) REFERENCES `task` (`task_id`),
+  ADD CONSTRAINT `booking_ibfk_4` FOREIGN KEY (`booking_status_id`) REFERENCES `booking_status` (`booking_status_id`);
+
+--
+-- Constraints for table `chat_member`
+--
+ALTER TABLE `chat_member`
+  ADD CONSTRAINT `chat_member_ibfk_1` FOREIGN KEY (`chat_id`) REFERENCES `chat` (`chat_id`),
+  ADD CONSTRAINT `chat_member_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `chat_message`
+--
+ALTER TABLE `chat_message`
+  ADD CONSTRAINT `chat_message_ibfk_1` FOREIGN KEY (`chat_id`) REFERENCES `chat` (`chat_id`),
+  ADD CONSTRAINT `chat_message_ibfk_2` FOREIGN KEY (`sender_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `guest`
+--
+ALTER TABLE `guest`
+  ADD CONSTRAINT `guest_ibfk_1` FOREIGN KEY (`wedding_plan_id`) REFERENCES `wedding_plan` (`wedding_plan_id`);
+
+--
+-- Constraints for table `our_wedding`
+--
+ALTER TABLE `our_wedding`
+  ADD CONSTRAINT `our_wedding_ibfk_1` FOREIGN KEY (`wedding_plan_id`) REFERENCES `wedding_plan` (`wedding_plan_id`);
+
+--
+-- Constraints for table `reset_password`
+--
+ALTER TABLE `reset_password`
+  ADD CONSTRAINT `reset_password_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `settings_theme`
+--
+ALTER TABLE `settings_theme`
+  ADD CONSTRAINT `settings_theme_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `task`
+--
+ALTER TABLE `task`
+  ADD CONSTRAINT `task_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`);
+
+--
+-- Constraints for table `vendor`
+--
+ALTER TABLE `vendor`
+  ADD CONSTRAINT `vendor_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`),
+  ADD CONSTRAINT `vendor_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `wedding_plan`
+--
+ALTER TABLE `wedding_plan`
+  ADD CONSTRAINT `wedding_plan_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `wedding_plan_task`
+--
+ALTER TABLE `wedding_plan_task`
+  ADD CONSTRAINT `wedding_plan_task_ibfk_1` FOREIGN KEY (`wedding_plan_id`) REFERENCES `wedding_plan` (`wedding_plan_id`),
+  ADD CONSTRAINT `wedding_plan_task_ibfk_2` FOREIGN KEY (`task_id`) REFERENCES `task` (`task_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
