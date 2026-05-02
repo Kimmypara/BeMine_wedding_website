02338a278dc74932f75d8bec76c8182b1c710209
Kimmypara <kparascandalo@gmail.com>
2026-05-02 15:28:35 +0200

    +planning page HTML

--- planning.php
+++ planning.php
@@ -11,7 +11,6 @@ if (!isset($_SESSION['user_id']) || (int)$_SESSION['role_id'] !== 2) {
 include "includes/nav.php";
 ?>
 
-
 <style>
 <?php include 'css/style.css'; ?>
 </style>
@@ -26,25 +25,95 @@ include "includes/nav.php";
 </head>
 <body>
     <main class="main-content">
+    <div class="container">
+
+<h1 class="title">Planning</h1>
+
+<img class="d-block w-100 mb-4" src="assets/images/planning_img.png" alt="Couple silhouette under stars">
+
+<form action="" method="POST">
+
+    <input class="form2" type="text" name="user_nickname" placeholder="Your Name" required><br>
+
+    <input class="form2" type="text" name="partner_nickname" placeholder="Partner’s Name" required><br>
+
+    <input class="form2" type="date" name="wedding_date" placeholder="Wedding date" required><br>
+
+    <h3 class="subtitle2">Select what you need for your wedding</h3>
+
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-12">
+               <label class="checkbox-item">
+  <input type="checkbox" name="categories[]" value="5">
+  <span>Ceremony Venue</span>
+</label>
+
+<label class="checkbox-item">
+  <input type="checkbox" name="categories[]" value="7">
+  <span>Reception Venue</span>
+</label>
+
+<label class="checkbox-item">
+  <input type="checkbox" name="categories[]" value="9">
+  <span>Bridal & Groom wear</span>
+</label>
+
+<label class="checkbox-item">
+  <input type="checkbox" name="categories[]" value="1">
+  <span>Florists</span>
+</label>
+
+<label class="checkbox-item">
+  <input type="checkbox" name="categories[]" value="4">
+  <span>Invitations</span>
+</label>
+
+<label class="checkbox-item">
+  <input type="checkbox" name="categories[]" value="10">
+  <span>Caterers & Beverages</span>
+</label>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12">
+            <label class="checkbox-item">
+  <input type="checkbox" name="categories[]" value="6">
+  <span>Videographers</span>
+</label>
+
+<label class="checkbox-item">
+  <input type="checkbox" name="categories[]" value="8">
+  <span>Photographers</span>
+</label>
+
+<label class="checkbox-item">
+  <input type="checkbox" name="categories[]" value="11">
+  <span>Fireworks</span>
+</label>
+
+<label class="checkbox-item">
+  <input type="checkbox" name="categories[]" value="12">
+  <span>Music</span>
+</label>
+
+<label class="checkbox-item">
+  <input type="checkbox" name="categories[]" value="13">
+  <span>Wedding Rings</span>
+</label>
+
+<label class="checkbox-item">
+  <input type="checkbox" name="categories[]" value="14">
+  <span>Beauty Services</span>
+</label>
 
 
+        </div>
+    </div>
 
 
-    
+</form>
+
+</div>
 </main>
 </body>
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
 
 <?php include "includes/footer.php"; ?>
